22e445730596c8d30cc7a56dded8985423c6ee90
liyu <liyu0x@outlook.com>
2023-06-01 10:39:33 +0800

    upload

--- check_32_result.py
+++ check_32_result.py
@@ -6,8 +6,8 @@ from new_task import check
 POOL = multiprocessing.Pool(processes=12)
 
 def varify():
-    #test_n = 2**8
-    test_n = 1
+    test_n = 2**22
+    #test_n = 1
     key = random.randint(0, 2**32)
     records = {}
     count = 0
@@ -22,6 +22,7 @@ def varify():
         task_list.append(
             POOL.apply_async(check, args=(x1, key))
         )
+    print(len(task_list))
     for task in task_list:
         result += task.get()
     if result == 0:
